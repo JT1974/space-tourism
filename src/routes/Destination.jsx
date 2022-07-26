d67582bd9530003738f839a015e5a9c5a4241119
JT1974 <takacsjan@gmail.com>
2022-07-26 11:20:28 +0200

    stylind completed

--- src/routes/Destination.jsx
+++ src/routes/Destination.jsx
@@ -10,137 +10,139 @@ const Main = styled.main`
 	gap: 2rem;
 	width: 100%;
 
-	@media screen and (min-width: 1440px) {
-		flex-direction: row;
-	}
-`
-
-const Hero = styled.section`
-	padding-top: 1.5rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 2rem;
-	width: 100%;
-
-	@media screen and (min-width: 768px) {
-		padding-top: 2.5rem;
-		padding-left: 2.5rem;
-	}
-`
-
-const Body = styled.section`
-	padding: 0 1.5rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 2rem;
-
-	.image-container {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-		align-items: flex-end;
-
-		img {
-			width: 170px;
-			height: 170px;
-		}
-	}
-
-	nav {
-		display: flex;
-		gap: 1.5rem;
-
-		button {
-			background: transparent;
-			border: none;
-			font-family: var(--sub-font-face);
-			font-size: 14px;
-			color: var(--button-idle-color);
-			line-height: 17px;
-			letter-spacing: 2.7px;
-			padding-bottom: 8px;
-			border-bottom: 3px solid transparent;
-			cursor: pointer;
-
-			&:hover {
-				border-bottom: 3px solid var(--title-color-hovered);
-			}
-
-			&.active {
-				color: var(--button-active-color);
-				border-bottom: 3px solid var(--button-active-color);
-			}
-		}
-	}
-
-	article {
+	.page-title {
+		padding-top: 1.5rem;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		padding-bottom: 2rem;
-		border-bottom: 1px solid var(--horizontal-line);
-
-		h2 {
-			color: var(--title-color);
-		}
-
-		p {
-			color: var(--text-color);
-		}
+		gap: 2rem;
+		width: 100%;
 	}
 
-	.planet-data {
-		padding: 0 3.5rem;
+	.page-body {
+		padding: 0 1.5rem;
 		display: flex;
 		flex-direction: column;
+		align-items: center;
 		gap: 2rem;
 
-		&--top,
-		&--btm {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			gap: 0.75rem;
-		}
-
-		.subHeading-1 {
-			color: var(--title-color);
-		}
-	}
-
-	@media screen and (min-width: 768px) {
-		padding: 0 6rem;
-
 		.image-container {
-			padding: 1.5rem 0;
+			width: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: flex-end;
 
 			img {
-				width: 300px;
-				height: 300px;
+				width: 170px;
+				height: 170px;
 			}
 		}
 
-		nav button {
-			font-size: 16px;
-			line-height: 19px;
+		nav {
+			display: flex;
+			gap: 1.5rem;
+
+			button {
+				background: transparent;
+				border: none;
+				font-family: var(--sub-font-face);
+				font-size: 14px;
+				color: var(--button-idle-color);
+				line-height: 17px;
+				letter-spacing: 2.7px;
+				padding-bottom: 8px;
+				border-bottom: 3px solid transparent;
+				cursor: pointer;
+
+				&:hover {
+					border-bottom: 3px solid var(--title-color-hovered);
+				}
+
+				&.active {
+					color: var(--button-active-color);
+					border-bottom: 3px solid var(--button-active-color);
+				}
+			}
 		}
 
 		article {
-			padding-bottom: 3rem;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			padding-bottom: 2rem;
+			border-bottom: 1px solid var(--horizontal-line);
 
 			h2 {
-				margin-bottom: 0.5rem;
+				color: var(--title-color);
+			}
+
+			p {
+				color: var(--text-color);
 			}
 		}
 
 		.planet-data {
 			padding: 0 3.5rem;
-			flex-direction: row;
-			justify-content: space-around;
-			width: 100%;
+			display: flex;
+			flex-direction: column;
+			gap: 2rem;
+
+			&--top,
+			&--btm {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				gap: 0.75rem;
+			}
+
+			.subHeading-1 {
+				color: var(--title-color);
+			}
 		}
+	}
+
+	@media screen and (min-width: 768px) {
+		.page-title {
+			padding-top: 2.5rem;
+			padding-left: 2.5rem;
+		}
+
+		.page-body {
+			padding: 0 6rem;
+
+			.image-container {
+				padding: 1.5rem 0;
+
+				img {
+					width: 300px;
+					height: 300px;
+				}
+			}
+
+			nav button {
+				font-size: 16px;
+				line-height: 19px;
+			}
+
+			article {
+				padding-bottom: 3rem;
+
+				h2 {
+					margin-bottom: 0.5rem;
+				}
+			}
+
+			.planet-data {
+				padding: 0 3.5rem;
+				flex-direction: row;
+				justify-content: space-around;
+				width: 100%;
+			}
+		}
+	}
+
+	@media screen and (min-width: 1440px) {
+		flex-direction: row;
 	}
 `
 
@@ -152,12 +154,12 @@ export default function Destination() {
 
 	return (
 		<Main>
-			<Hero>
+			<section className='page-title'>
 				<PageTitle>
 					<span>01</span> Pick your destination
 				</PageTitle>
-			</Hero>
-			<Body>
+			</section>
+			<section className='page-body'>
 				<div className='image-container'>
 					<img src={data?.images.webp} alt={data?.name} />
 				</div>
@@ -192,7 +194,7 @@ export default function Destination() {
 						<span className='subHeading-1'>{data?.travel}</span>
 					</div>
 				</div>
-			</Body>
+			</section>
 		</Main>
 	)
 }
